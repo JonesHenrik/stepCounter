f3192c7a4fece6c132fb1e59499639e5a1d5a847
JonesHenrik <henrikjones22@gmail.com>
2024-07-15 16:31:05 -0400

    Began setting up healthKit

---
+++ stepCounter/RequestSteps.swift
@@ -0,0 +1,19 @@
+//
+//  RequestSteps.swift
+//  stepCounter
+//
+//  Created by Henrik Jones on 7/15/24.
+//
+
+import Foundation
+import HealthKit
+
+// https://developer.apple.com/documentation/healthkit/setting_up_healthkit
+
+class stepViewController {
+//    if HKHealthStore.isHealthDataAvailable() {
+//
+//    }
+    
+    let healthStore = HKHealthStore()
+}
