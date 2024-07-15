da4f64970a9aed8692ed7ab8bda4270df7c4901c
JonesHenrik <henrikjones22@gmail.com>
2024-07-15 15:31:35 -0400

    Initial Commit

---
+++ stepCounter/stepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepCounterApp.swift
+//  stepCounter
+//
+//  Created by Henrik Jones on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct stepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
